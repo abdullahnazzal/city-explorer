460013a3e611e889f92e561eafc13b69fb7b1470
abdullahnazzal <a.nazzal1995@gmail.com>
2021-08-17 14:04:24 +0300

    Fix the data

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ class App extends React.Component {
       cites: {},
       cite: "",
       flag: false,
-      lon:"",
-      lat:"",
-      data:""
+      lon: "",
+      lat: "",
+      data: []
     }
 
   }
@@ -24,31 +24,33 @@ class App extends React.Component {
     await this.setState({
       cite: e.target.cite.value
     })
-// console.log("asdnasjdnaskd saofns"+process.env.REACT_APP_LOCATIONIQ_KEY);
+    // console.log("asdnasjdnaskd saofns"+process.env.REACT_APP_LOCATIONIQ_KEY);
 
 
 
-//http://localhost:3001/weather?searchQuery=Seattle&lat=47.60621&lon=-122.33207
+    //http://localhost:3001/weather?searchQuery=Seattle&lat=47.60621&lon=-122.33207
     let loc = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.cite}&format=json`;
 
-    
+
 
     let result = await axios.get(loc);
-    // console.log(result.data[0]);
+    console.log(result.data[0]);
 
     await this.setState({
       cites: result.data[0],
-      lat:result.data[0].lat,
-      lon:result.data[0].lon,
+      lat: result.data[0].lat,
+      lon: result.data[0].lon,
       flag: true
     })
-    let loc2 = `http://localhost:3001/weather?searchQuery=${this.state.cite}&lat=${this.state.lat}&lon=${this.state.lon}`;
-
+    let loc2 = `${process.env.REACT_APP_SERVER_LINK}/weather?searchQuery=${this.state.cite}&lat=${this.state.lat}&lon=${this.state.lon}`;
+console.log("this.state.citethis.state.cite"+loc2);
     let result2 = await axios.get(loc2);
-    console.log(this.state.cite , this.state.lat, this.state.lon);
-    console.log("result2result2result2result2"+result2);
-    this.setState({
-      data:result2
+    console.log(this.state.cite, this.state.lat, this.state.lon);
+    console.log("result2result2result2result2" + result2);
+    console.log("result2result2result2result2" + result2.data);
+
+    await this.setState({
+      data: result2.data
     })
   }
 
@@ -63,16 +65,16 @@ class App extends React.Component {
             <button>Explore!</button>
           </form>
 
-          <CardInfo 
-          cites={this.state.cites}
-          cite={this.state.cite}
-          flag={this.state.flag}
-          
+          <CardInfo
+            cites={this.state.cites}
+            cite={this.state.cite}
+            flag={this.state.flag}
+
           />
 
-          <Weather 
-          flag={this.state.flag}
-          info={this.state.data}
+          <Weather
+            flag={this.state.flag}
+            info={this.state.data}
           />
           {/* {this.state.flag && <p>{this.state.cite} lat:{this.state.cites.lat} loc:{this.state.cites.lon}</p>} */}
         </>
